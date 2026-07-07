6e79ece876bddac1387791848e496ff6578b36ff
sarahsiqueira777 <anakelleycarril@gmail.com>
2026-07-07 16:43:39 -0400

    aula4

--- FUNDAMENTOS JAVA/Carro.java
+++ FUNDAMENTOS JAVA/Carro.java
@@ -1,19 +1,48 @@
 // Herança: Carro é uma subclasse de Automóvel
 public class Carro extends Automovel {
+    // Atrbutos específicos e esclusivos da classe carro
+    //Criar dois novos atributos para a classe carro
     // Atributos protegidos
-    private String marca;
-    private String modelo;
-    private int ano;
-    private double preço;
-    private boolean motorLigado;
+    private int quantidadePortas;
+    private String tipoCarroceria; // ex: Sedan, Hatch, Suv, Cupê
 
-    public Carro (String marca, String modelo, int ano, double preco, boolean motorLigado){
-        super(marca, modelo, an , preco);
-        this.marca = marca;
-        this.modelo = modelo;
-        this.ano = ano;
-        this.preco = preco;
-        this.motorLigado = motorLigado; // Atributo booleano recebe o valor passado
+    // Construtor da subclasse carro
+
+
+    public Carro(int quantidadePortas, String tipoCarroceria, String marca, String modelo, int ano, double preco) {
+        //Envia obrigatoriamente os dados genericos para o construtor da superclasse (Automovel) 
+        super(marca, modelo, ano, preco);
+
+        //Inicializa as propriedades que pertencem apenas ao Carro
+        this.quantidadePortas = quantidadePortas;
+        this.tipoCarroceria = tipoCarroceria;
     }
-    
+
+    //Métodos de comportamento exclusivo do carro de passeio
+    public void abrirPortaMalas(){
+        System.out.println("O porta-malas do" + getModelo() + " (" + this.tipoCarroceria + ") foi aberto pelo controle remoto."); 
+    }
+
+    // Sobrescrita de Método (Polimorfismo): Adaptando a Ficha Técnica para as características de um Carro
+@Override
+     public void exibirFichaTecnica() {
+
+    // Invoca o comportamento padrão escrito em Automovel (Evita duplicação de código)
+    super.exibirFichaTecnica();
+
+    // Adiciona informações específicas do veículo de passeio
+    System.out.println("--- Especificações de Veículo de Passeio ---");
+    System.out.println("Quantidade de Portas: " + this.quantidadePortas);
+    System.out.println("Tipo de Carroceria: " + this.tipoCarroceria);
+    System.out.println("=========================================\n");
+}
+
+// Getters e Setters específicos
+public int getQuantidadePortas() { return quantidadePortas; }
+
+public void setQuantidadePortas(int quantidadePortas) { this.quantidadePortas = quantidadePortas;}
+
+public String getTipoCarroceria() { return tipoCarroceria;}
+
+public void setTipoCarroceria(String tipoCarroceria) { this.tipoCarroceria = tipoCarroceria; }
 }
--- FUNDAMENTOS JAVA/MainHeranca.java
+++ FUNDAMENTOS JAVA/MainHeranca.java
@@ -1,3 +1,46 @@
 public class MainHeranca {
+
+    public static void main(String[] args) {
+
+        System.out.println("Sistema Integrado da Concessionária - Teste de Herança de Veículos\n");
+
+        // 1. Instanciando um Caminhão (Subclasse 1)
+        Caminhao caminhaoGrande = new Caminhao("Scania", "R 540", 2024, 720000.00, 80.0, 6);
+
+        // 2. Instanciando diferentes Carros de Passeio (Subclasse 2)
+        // Aqui demonstramos os exemplos de carroceria: SUV, Sedan, Hatch e Cupê
+        Carro carroSUV = new Carro(4, "SUV", "Honda", "HR-V", 2025, 160000.0);
+        Carro carroSedan = new Carro(4, "Sedan", "toyota", "Corola", 2024, 150000.00);
+        Carro carroHatch = new Carro(4, "Hatch", "Chevrolet", "Onix", 2023, 85000.0);
+        Carro carroCupe = new Carro(4, "Cupe", "Porche", "911 Carrera", 2026, 900000.0);
+
+        // 3. Testando interações com os Carros (Métodos herdados e exclusivos)
+        System.out.println("=== Interagindo com os Carros ===");
+
+        carroSUV.ligarMotor();       // Método herdado de Automóvel
+        carroSUV.abrirPortaMalas();  // Método exclusivo de Carro
+
+        System.out.println();
+        carroHatch.ligarMotor();
+        carroHatch.atualizarPreco(87000.00); //Método herdado de Automovel
+
+        System.out.println();
+        carroCupe.abrirPortaMalas();
+
+
+        // 4. Testando o Caminhão para contrapor com as regras do Carro
+        System.out.println("\n=== Interagindo com o Caminhão ===");
+        caminhaoGrande.ligarMotor();
+        caminhaoGrande.carregarCarga(45.0); // Método exclusivo de Caminhão
+
+        // 5. Exibindo as Fichas Técnicas Personalizadas (Polimorfismo em Ação)
+       System.out.println("\n=== FICHAS TÉCNICAS DOS CARROS DE PASSEIO ===");
+       carroSUV.exibirFichaTecnica();
+       carroSedan.exibirFichaTecnica();
+       carroHatch.exibirFichaTecnica();
+       carroCupe.exibirFichaTecnica();
+    }
     
+ 
+
 }
