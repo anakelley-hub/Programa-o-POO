f03382f4289e6307ffe2ae390a389262ab9c454c
sarahsiqueira777 <anakelleycarril@gmail.com>
2026-07-08 16:46:43 -0400

    aula05

---
+++ FUNDAMENTOS JAVA/Exercicio01.java
@@ -0,0 +1,10 @@
+// Imprima todos os numeros de 150 a 300.
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+        for (int i = 150; i <= 300; i++) {
+            System.out.println(i); // Imprima todos os números de 150 a 300.
+        }
+        
+    }
+}
---
+++ FUNDAMENTOS JAVA/Exercicio02.java
@@ -0,0 +1,13 @@
+// Imprima todos os números de 1 a 100.
+
+public class Exercicio02 {
+    public static void main (String[] args) {
+        int soma = 0;
+        for(int i = 1; i <= 100; i++) {
+            soma += i;
+        }
+        System.out.println(" A soma de 1 a 100 é: " + soma);
+    }
+}
+    
+
